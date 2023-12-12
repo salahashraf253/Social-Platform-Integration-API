34682eaf372287885d70e79488ac56fed3cd93f8
Salah Ashraf <salahashraf924@gmail.com>
2023-12-12 15:12:38 +0200

    Initial Commit

---
+++ src/test/java/com/platform/integration/SocialPlatformInetgrationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platform.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialPlatformInetgrationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
