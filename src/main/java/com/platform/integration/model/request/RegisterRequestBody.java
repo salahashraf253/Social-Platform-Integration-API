13bba313c0a56820312ef0796fae0fd5f0a5f41b
Salah Ashraf <salahashraf924@gmail.com>
2023-12-15 21:05:16 +0200

    feat: add constraints for attributes of RegisterRequestBody

--- src/main/java/com/platform/integration/model/request/RegisterRequestBody.java
+++ src/main/java/com/platform/integration/model/request/RegisterRequestBody.java
@@ -1,9 +1,11 @@
 package com.platform.integration.model.request;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 
 @Data
 @Builder
@@ -11,9 +13,22 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RegisterRequestBody {
 
+    @NotNull(message = "firstName is required")
+    @NotEmpty(message = "firstName can't be empty")
     private String firstName;
+
+    @NotNull(message = "lastName is required")
+    @NotEmpty(message = "lastName can't be empty")
     private String lastName;
+
+    @NotNull(message = "email cis required")
+    @NotEmpty(message = "email can't be empty")
+    @Email(message = "email format is invalid")
     private String email;
+
+    @NotNull(message = "password is required")
+    @NotEmpty(message = "password can't be empty")
+    @Size(min = 8, message = "minimum length of password is 8")
     private String password;
 
 }
